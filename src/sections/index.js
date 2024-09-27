d8dd43c2f3573e9e23355d62e7430748ef5a0ebf
obaidallah1 <obaidalqershi1@gmail.com>
2024-09-27 20:30:28 +0300

    main commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+
+import Hero from './Hero'
+import PopularProducts from './PopularProducts'
+import SuperQuality from './SuperQuality'
+import Services from './Services'
+import SpecialOffers from './SpecialOffers'
+import Subscribe from './Subscribe'
+import CustomerReviews from './CustomerReviews'
+import Footer from './Footer'
+export{
+    Hero,
+    PopularProducts,
+    Subscribe,
+    SuperQuality,
+    Services,
+    SpecialOffers,
+    CustomerReviews,
+    Footer,
+}
